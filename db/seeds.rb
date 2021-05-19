3b9b8839f81d150a6eaea57e042450f5cb0bb4bc
Delphine Picard <delphinepicard.sk@gmail.com>
2021-05-19 17:06:03 +0200

    step2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# puts "Cleaning database..."
-# Restaurant.destroy_all
+puts "Cleaning database..."
+Restaurant.destroy_all
 
-# puts 'Creating restaurants...'
-# ti_parisien = { name: 'TiParisien', address: '7 rue Marcel, Paris 7', phone_number: '02.33.25.40.32', category: 'french'}
-# sushis_folies = { name: 'Sushis folies', address: '56 rue Guichard, Saint Etienne', phone_number: '02.33.25.40.33', category: 'japanese'}
-# vive_le_riz = { name: 'Vive le Riz', address: '7 rue des Capucins, Lyon', phone_number: '02.33.25.40.34', category: 'chinese'}
-# pizza_east =  { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ',phone_number: '02.33.25.40.35', category: 'italian'}
-# ma_crêperie = { name: 'Ma Crêperie', address: '7 Boundary St, New York',phone_number: '02.33.25.40.36', category: 'belgian'}
+puts 'Creating restaurants...'
+ti_parisien = { name: 'TiParisien', address: '7 rue Marcel, Paris 7', phone_number: '02.33.25.40.32', category: 'french'}
+sushis_folies = { name: 'Sushis folies', address: '56 rue Guichard, Saint Etienne', phone_number: '02.33.25.40.33', category: 'japanese'}
+vive_le_riz = { name: 'Vive le Riz', address: '7 rue des Capucins, Lyon', phone_number: '02.33.25.40.34', category: 'chinese'}
+pizza_east =  { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '02.33.25.40.35', category: 'italian'}
+ma_crêperie = { name: 'Ma Crêperie', address: '7 Boundary St, New York', phone_number: '02.33.25.40.36', category: 'belgian'}
 
-# [ti_parisien, sushis_folies, vive_le_riz, pizza_east, ma_crêperie ].each do |attributes|
-#   restaurant = Restaurant.create!(attributes)
-#   puts 'Created #{restaurant.name}'
-# end
-# puts 'Finished!'
+[ti_parisien, sushis_folies, vive_le_riz, pizza_east, ma_crêperie].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
